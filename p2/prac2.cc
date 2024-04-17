626474ed71e51ad8b69d84388735be19b0fc82cc
lms121-ua <124355409+lms121-ua@users.noreply.github.com>
2024-04-17 12:47:14 +0200

    Update prac2.cc 1

--- p2/prac2.cc
+++ p2/prac2.cc
@@ -1,4 +1,144 @@
 // Programación 2 - Práctica 2
-// DNI:
-// Nombre:
+// DNI: x8594224k
+// Nombre: Letisia Monica Sandu
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int KMAXNAME = 50;
+const int KMAXPASSWORD = 5;
+
+// Registro para las preguntas
+struct Question{
+    unsigned int id;
+    unsigned int unit;
+    string question;
+    string answer;
+};
+
+// Registro para los profesores
+struct Teacher{
+    char name[KMAXNAME];
+    char password[KMAXPASSWORD];
+    unsigned int answered;
+};
+
+// Registro para la base de datos
+struct Database{
+    unsigned int nextId;
+    vector<Question> questions;
+    vector<Teacher> teachers;
+};
+
+// Tipos de errores posibles
+enum Error {
+    ERR_OPTION,
+    ERR_EMPTY,
+    ERR_UNIT,
+    ERR_CHAR,
+    ERR_FILE,
+    ERR_NAME,
+    ERR_DUPLICATED,
+    ERR_PASSWORD,
+    ERR_NO_QUESTIONS,
+    ERR_ID,
+    ERR_ARGS
+};
+
+/* Función que muestra los mensajes de error
+e: tipo de error a mostrar
+return: nada
+*/
+void error(Error e){
+    switch (e){
+        case ERR_OPTION:
+            cout << "ERROR: wrong option" << endl;
+            break;
+        case ERR_EMPTY:
+            cout << "ERROR: empty string" << endl;
+            break;
+        case ERR_UNIT:
+            cout << "ERROR: wrong unit" << endl;
+            break;
+        case ERR_CHAR:
+            cout << "ERROR: wrong char" << endl;
+            break;
+        case ERR_FILE:
+            cout << "ERROR: cannot open file" << endl;
+            break;
+        case ERR_NAME:
+            cout << "ERROR: wrong teacher name" << endl;
+            break;
+        case ERR_DUPLICATED:
+            cout << "ERROR: duplicated name" << endl;
+            break;
+        case ERR_PASSWORD:
+            cout << "ERROR: wrong password" << endl;
+            break;      
+        case ERR_NO_QUESTIONS:
+            cout << "ERROR: no pending questions" << endl;
+            break;
+        case ERR_ID:
+            cout << "ERROR: wrong id" << endl;
+            break;
+        case ERR_ARGS:
+            cout << "ERROR: wrong arguments" << endl;
+    }
+}
+
+/* Función que muestra el menú de opciones
+return: nada
+*/
+void showMenu() {
+    cout << "1- Add question" << endl
+         << "2- Batch add questions" << endl
+         << "3- Delete question" << endl
+         << "4- Add teacher" << endl
+         << "5- Add answers" << endl
+         << "6- View answers" << endl
+         << "7- View statistics" << endl
+         << "8- Export questions" << endl
+         << "q- Quit" << endl
+         << "Option: ";
+}
+
+// Función principal. Tendrás que añadir más código tuyo
+int main(int argc, char *argv[]) {
+    Database data;
+    data.nextId=1;
+    char option;
+    
+    do{
+        showMenu();
+        cin >> option;
+        cin.get();
+        
+        switch(option){
+            case '1': // Llamar a la función "addQuestion" para añadir una nueva pregunta
+                break;
+            case '2': // Llamar a la función "batchAddQuestion" para añadir preguntas desde fichero
+                break;
+            case '3': // Llamar a la función "deleteQuestion" para eliminar una pregunta
+                break;
+            case '4': // Llamar a la función "addTeacher" para añadir un nuevo profesor
+                break;
+            case '5': // Llamar a la función "addAnswers" para añadir respuestas a las preguntas
+                break;
+            case '6': // Llamar a la función "viewAnswers" para mostrar las preguntas con sus respuestas
+                break;
+            case '7': // Llamar a la función "viewStatistics" para ver las estadísticas de las preguntas
+                break;
+            case '8': // Llamar a la función "exportQuestions" para guardar las preguntas en fichero
+                break;
+            case 'q': // Salir del programa guardando los profesores en fichero
+                break;
+            default: error(ERR_OPTION);
+        }
+    }while(option!='q');
+
+  return 0;
+}
+
 
