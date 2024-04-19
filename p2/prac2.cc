fad7c2859f40bf52fc58883eb4feb70ea35568bb
lms121-ua <124355409+lms121-ua@users.noreply.github.com>
2024-04-19 18:27:19 +0200

    Update prac2.cc 5

--- p2/prac2.cc
+++ p2/prac2.cc
@@ -285,7 +285,190 @@ string askTeacher(const vector<Teacher> &teachers){
     }while(name != "" && (!ok_name || pos != -1));
     return name;
 }
+bool comprobateTeacherName(string name){
+    bool correcto;
+    int i, tam;
+    
+    correcto = true;
+    i = 0;
+    tam = name.length();
+    
+    while(i < tam && correcto){ //condicion que verifica si cada iteracion de la cadena son letras o espacios en blanco
+        if(name[i] != ' ' && !isalpha(name[i])){ //condicion que verifica si el caracter de la posicion actual NO es un
+                                                  // espacio en blanco y NO es una letra
+            correcto = false;
+        
+        }else{
+            i++; //y si es valido pasa al siguiente caracter
+        }
+    }
 
+    return correcto; //devuelve correcto que valida el nombre o no
+}
+
+//funcion que se encargar de buscar, en un vector de profesor, a un profe por su nombre
+int buscarTeacherName(const vector<Teacher> &teachers, string name){
+    int pos, i;
+    
+    pos = -1;
+    i = 0;
+
+    while(i < teachers.size() && pos == -1){ //bucle que se repite mientras haya mas profes por revisar y no se haya encontrado al profe
+        if(teachers[i].name == name){ //condicion que verifica si el nombre del profe buscado es igual al nombre del profe de la posicion actual
+            pos = i; //y si coincide, se actualiza la posicion
+        }
+        i++; //y pasa al siguiente profesor
+    }
+    
+    return pos; //devuelve la posicion del profe encontrado o -1 si no se ha encontrado
+}
+
+//funcion que se encarga de pedir al usuario el nombre de un profesor y luego lo valida
+string askTeacher(const vector<Teacher> &teachers){
+    string nombre;
+    int pos;
+    bool nombre_correcto;
+
+    pos = -1;
+    nombre_correcto = false;
+
+    do{
+        cout << "Enter teacher name: ";
+        getline(cin, nombre);
+        
+        if(nombre == ""){ //condicion que verifica si la entrada esta vacia
+            error(ERR_EMPTY);
+        
+        }else{
+            nombre_correcto = comprobateTeacherName(nombre); //y luego se verifica si el nombre es valido con otra funcion
+            
+            if(!nombre_correcto){
+                error(ERR_NAME);
+            
+            }else{
+                nombre = deleteSpaces(nombre); //si el nombre es valido se borran los espacios en blanco
+                
+                if(nombre.length() < 3 || nombre.length() >= KMAXNAME){ //condicion que verifica si el nombre tiene la longitud correcta
+                    error(ERR_NAME);
+                    nombre_correcto = false;
+                
+                }else{
+                    pos = buscarTeacherName(teachers, nombre);// y si la tiene se busca al profe en el vector de profesores
+                    
+                    if(pos != -1){ //condicion que verifica si el profesor ya existe en el vector
+                        error(ERR_DUPLICATED); //y si existe muestra mensaje de error
+                    }
+                }
+            }
+        }
+    }while(nombre != "" && (!nombre_correcto || pos != -1)); //bucle que se repite mientras el nombre NO este vacio y NO sea valido
+    
+    return nombre;
+}
+
+//funcion que se encarga de validar una contrasenya
+//condiciones: que tenga solo 4 caracteres y sean solo digitos
+bool testPassword(string password){
+    bool correcto;
+    int i, tam;
+
+    tam = password.length();
+    if(tam == 4){  //condicion que verifica si el tamanyo de la contrasenya es de 4 caracteres
+        correcto = true;
+        
+        for(int i = 0; i < tam && correcto; i++){ //bucle que recorre la contrasenya comprobando si los caracteres son digitos
+            if(!isdigit(password[i])){ //condicion que verifica si el caracter actual NO es un digito
+                correcto = false;
+            }
+        }
+    }else{
+        correcto = false; 
+    }   
+    return correcto; //devuelve correcto, que indica que la contrasenya es valida
+}
+
+//funcion que se encarga de pedir y validar una contrasenya
+string askPassword(){
+    string password;
+    bool correcta;
+    
+    do{
+        cout << "Enter password: ";
+        getline(cin, password);
+        
+        if(password.empty()){ //condicion que comprueba si la entrada esta vacia
+            error(ERR_EMPTY);
+        
+        }else{
+            correcta = testPassword(password); //sino se verifica si la contrasenya es valida
+            
+            if(!correcta){ //y si no es valida muestra mensaje de error
+                error(ERR_PASSWORD);
+            }
+        }
+    }while(!correcta && !password.empty()); //bucle que se repite mientras la contrasenya NO es valida y NO esta vacia
+    
+    return password;
+}
+/*
+string code(string sp){
+    string aux = "";
+    char codigos[] = "TRWAGMYFPD";
+    int i = 0;
+    while(i < sp.length()){
+        aux += codigos[sp[i] - '0'];
+        i++;
+    }
+    return aux;
+}*/
+
+//funcion que se encarga de generar un codigo a partir de una cadena de digitos
+string code(string cadena){
+    string aux;
+    char codigos[] = "TRWAGMYFPD"; //codigos correspondientes a cada numero
+    int i;
+    
+    i = 0;
+    aux = ""; //la inicializamos vacia
+
+    for(i = 0; i < cadena.length(); i++){ //bucle que recorre la cadena de numeros 
+        aux = aux + codigos[cadena[i] - '0'];
+    }
+
+    return aux; //devuelve codigo generado
+}
+
+//funcion que se encarga de anyadir un nuevo profe
+void addTeacher(Database &data){
+    Teacher profe;
+    string nombre, contrasenya, c_correcta;
+    
+    nombre = askTeacher(data.teachers); //se pide el nombre de un nuevo profe y se valida
+    
+    if(nombre != ""){ //condicion que verifica que el nombre no este vacio
+        contrasenya = askPassword(); //continua pidiendo la contrasenya y la valida
+        
+        if(contrasenya != ""){ //condicion que verifica que la contrasenya no este vacia
+            c_correcta = code(contrasenya); //se genera el codigo para la contrasenya
+
+            //se copia el nombre y la contrasenya al objeto Teacher
+            strcpy(profe.name, nombre.c_str());
+            strcpy(profe.password, c_correcta.c_str());
+            profe.answered = 0;
+            data.teachers.push_back(profe); //se anyade el nuevo profe a la base de datos
+        }
+    }   
+}
+
+//funcion que se encarga de mostrar las preguntas que no tengan respuestas
+void showQuestions(const vector<Question> &questions){
+    for(int i = 0; i < questions.size(); i++){ //bucle que recorre cada pregunta
+        if(questions[i].answer == ""){ //condicion que verifica si la pregunta actual tiene respuesta
+            cout << questions[i].id << ". (" << questions[i].unit << ") " << questions[i].question;
+            cout << endl;
+        }
+    }
+}
 // Función principal. Tendrás que añadir más código tuyo
 int main(int argc, char *argv[]) {
     Database data;
